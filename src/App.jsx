0a1bca9cffcd0d3772f07ebf5d448a9b6e7531f0
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-06 20:45:49 -0300

    Editing the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo App</h1>
+      <TodoList />
     </div>
   );
 }
